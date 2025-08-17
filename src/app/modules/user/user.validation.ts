d92b8b6c9c95063c6a89ed4802a3c25421e76aad
Rohithossain12 <https://github.com/Rohithossain12>
2025-08-17 17:47:31 +0600

    some modified

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -2,22 +2,33 @@ import z from "zod";
 import { IsActive, Role } from "./user.interface";
 
 export const createUserZodSchema = z.object({
-    name: z.string({ error: "Name Must be String" }).min(5, { message: "Name to short minimum 5 character long" }).max(30, { message: "Name to long" }),
-    email: z.string({ error: "Email must be string" })
-        .email({ message: "Invalid email address format" })
-        .min(5, { message: "Email must be at lest 5 character long" })
-        .max(100, { message: "Email can not exceed 100 characters" }),
-    password: z.string().min(8)
-        .regex(/^(?=.*[A-Z])/, { message: "Password must contain at least 1 uppercase" })
-        .regex(/^(?=.*[!@#$%^&*])/, { message: "Password must contain at least 1 special character" })
-        .regex(/^(?=.*\d)/, { message: "Password must contain at lest 1 number" }),
-    phone: z.string({ error: "Phone Number must be String" })
-        .regex(/^(?:\+?88)?01[3-9]\d{8}$/, { message: "Please provide a valid Bangladeshi phone number." })
-        .optional(),
-    address: z.string({ error: "Address must be string" })
-        .max(200, { message: "Address can not exceed 200 characters" })
-        .optional()
-
+  provider: z.enum(["google", "credentials"]),
+  name: z.string({ error: "Name Must be String" }).min(5, { message: "Name to short minimum 5 character long" }).max(30, { message: "Name to long" }),
+  email: z.string({ error: "Email must be string" })
+    .email({ message: "Invalid email address format" })
+    .min(5, { message: "Email must be at lest 5 character long" })
+    .max(100, { message: "Email can not exceed 100 characters" }),
+  password: z.string()
+    .min(8)
+    .regex(/^(?=.*[A-Z])/, { message: "Password must contain at least 1 uppercase" })
+    .regex(/^(?=.*[!@#$%^&*])/, { message: "Password must contain at least 1 special character" })
+    .regex(/^(?=.*\d)/, { message: "Password must contain at lest 1 number" })
+    .optional(),     
+  phone: z.string({ error: "Phone Number must be String" })
+    .regex(/^(?:\+?88)?01[3-9]\d{8}$/, { message: "Please provide a valid Bangladeshi phone number." })
+    .optional(),
+  address: z.string({ error: "Address must be string" })
+    .max(200, { message: "Address can not exceed 200 characters" })
+    .optional()
+}).refine((data) => {
+  
+  if (data.provider === "credentials" && !data.password) {
+    return false;
+  }
+  return true;
+}, {
+  message: "Password is required when provider is credentials",
+  path: ["password"]
 });
 
 
